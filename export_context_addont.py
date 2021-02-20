1a2f810066849053136a97d9a68464747193b6c9
nikogoli <49331838+nikogoli@users.noreply.github.com>
2021-02-21 01:49:22 +0900

    アドオンの修正

--- export_context_addont.py
+++ export_context_addont.py
@@ -20,6 +20,21 @@ bl_info = {
 	"category": "Custom"
 }
 
+# アドオン設定
+class AddonPreferences(bpy.types.AddonPreferences):
+	bl_idname = __name__
+
+	data_dir : StringProperty(name="Directory: Editors' json files", default="temp", subtype='FILE_PATH')
+	out_dir : StringProperty(name="Directory: Combined json file", default="outcome", subtype='FILE_PATH')
+	name : StringProperty(name="Combined Json File Name", default="Cubeを選択.json")
+
+	def draw(self, context):
+		lout = self.layout
+		lout.use_property_split = True
+		for p in ['data_dir', 'out_dir', 'name']:
+			lout.prop(self, p)
+		lout.operator(CombineJsonOperator.bl_idname).name = self.name
+
 
 # オペレーター
 class ExportContextOperator(bpy.types.Operator):
@@ -28,19 +43,22 @@ class ExportContextOperator(bpy.types.Operator):
 	bl_options = {'REGISTER', 'UNDO'}
 
 	def execute(self, context):
+		prefs = bpy.context.preferences.addons[__name__].preferences
+
 		sd_name = context.space_data.bl_rna.name
 		if sd_name == "Space":
 			file_name = "Topbar.json"
 		else:
 			sd_name = sd_name.replace("Space ", "").replace(" Space", "")
-			file_name = f"{sd_name}.json".
+			file_name = f"{sd_name}.json"
 
-		file_path = Path(prefs.data_dir) / file_name
+		file_path = Path.cwd() / prefs.data_dir / file_name
 		override = context.copy()
 		out_dic = {k: str(override[k]) for k in override.keys()}
 
-		with out_path.open("w", encoding='utf-8') as fw:
+		with file_path.open("w", encoding='utf-8') as fw:
 			json.dump(out_dic, fw, ensure_ascii=False)
+
 		self.report({'INFO'}, f"Save {file_name}")
 		return {'FINISHED'}
 
@@ -54,36 +72,24 @@ class CombineJsonOperator(bpy.types.Operator):
 
 	def execute(self, context):
 		prefs = bpy.context.preferences.addons[__name__].preferences
-		data_dir = Path(prefs.data_dir)
+		data_dir = Path.cwd() / prefs.data_dir
 		json_paths = [p for p in data_dir.iterdir() if p.is_file()]
 		main_dic = {}
 		for jp in json_paths:
 			with jp.open("r", encoding="utf-8") as fr:
 				main_dic[jp.stem] = json.load(fr)
 
-		out_path = Path(prefs.out_dir) / f"{name}.json"
-		with out_data_path.open("w", encoding='utf-8') as fw:
+		out_path = Path.cwd() / prefs.out_dir / f"{self.name}"
+		with out_path.open("w", encoding='utf-8') as fw:
 			json.dump(main_dic, fw, ensure_ascii=False)
-		self.report({'INFO'}, f"Combine {len(json_paths)} files to {name}.json")
+
+		self.report({'INFO'}, f"Combine {len(json_paths)} files to {self.name}")
 		return {'FINISHED'}
 
 
 def menu_func(self, context):
-    self.layout.operator(MyVariousOperator.bl_idname, text="CTX", icon='EXPORT')
+    self.layout.operator(ExportContextOperator.bl_idname, text="CTX", icon='EXPORT')
 		
-
-# アドオン設定
-class AddonPreferences(bpy.types.AddonPreferences):
-	bl_idname = __name__
-
-	data_dir : StringProperty(name="Directory Path: Editors' json files", default=str(Path.cwd() / "temp"), subtype='FILE_PATH')
-	out_dir : StringProperty(name="Directory Path: Combined json file", default=str(Path.cwd() / "outcome"), subtype='FILE_PATH')
-	name : StringProperty(name="Combined Json File Name", default="Cubeを選択.json")
-
-	def draw(self, context):
-		for p in ['data_dir', 'out_dir', 'name']:
-			self.layout.prop(self, p)
-		self.operator(CombineJsonOperator.bl_idname).name = self.name
 
 #---------------------------------------------------
 
