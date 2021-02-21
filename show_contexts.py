e63c8a59c8241fbe21a715a183193244a6c90e20
nikogoli <49331838+nikogoli@users.noreply.github.com>
2021-02-21 11:09:57 +0900

    エディタ基準で比較モード：とりあえず追加

--- show_contexts.py
+++ show_contexts.py
@@ -24,25 +24,26 @@ context_DATA_dic = {p.stem:p for p in Path(Home_path, "outcome").iterdir() if p.
 Info_path = Home_path / "info" / "contexts_by_doc.json"
 with Info_path.open("r", encoding='utf-8') as fr:
 	ROW_IN_DOC = json.load(fr)
-COL_NAMES = [None] * 16
-ROW_NAMES = []
+COL_NAMES = ['3D View','Clip Editor','Console','Dope Sheet Editor','File Browser','Graph Editor','Image Editor','Info','Nla Editor','Node Editor','Outliner','Preferences','Properties','Sequence Editor','Text Editor','Topbar']
+ROW_NAMES = list(set(sum([v for v in ROW_IN_DOC.values()], []))).sort()
 
 
-def extract_diff_dict(Table, Row_names, Col_names):
+def extract_diff_dict(Table):
 	out_dic = {}
-	for rn in Row_names:
+	colnames = Table.columns.tolist()
+	for rn in Table.index.tolist():
 		if rn in ["area", "region", "space_data", "active_operator"]:
 			continue
 		values = Table.loc[rn].tolist()
 		if len(set(values)) >= 2:
 			temp_dic = {x:[] for x in list(set(values))}
-			for cn, v in zip(Col_names, values):
+			for cn, v in zip(colnames, values):
 				temp_dic[v].append(cn)
 			out_dic[rn] = temp_dic
 
 	for key in out_dic.keys():
 		for kk in out_dic[key].keys():
-			if len(out_dic[key][kk]) == len(Col_names)-1:
+			if len(out_dic[key][kk]) == len(colnames)-1:
 				out_dic[key][kk] = "他の全て"
 			elif len(out_dic[key][kk]) == 1:
 				out_dic[key][kk] = out_dic[key][kk][0]
@@ -74,36 +75,6 @@ def compare_list(Pre_list_text, Act_list_text):
 		add = list(added)[0] if len(added)==1 else list(added)
 		text = f"[{','.join(pre_list)}] において {sub} が除かれ {add} が加わった"
 	return text
-
-
-@st.cache
-def get_data(Data_name):
-	with context_DATA_dic[Data_name].open("r", encoding='utf-8') as fr:
-		main_dic = json.load(fr)
-	COL_NAMES = sorted(list(main_dic.keys()))
-	all_context_keys = sum([list(main_dic[c].keys()) for c in COL_NAMES], [])
-	ROW_NAMES = list(set(all_context_keys))
-
-	temp_dic = {x:[] for x in ROW_NAMES}
-	for cn in COL_NAMES:
-		context = main_dic[cn]
-		for rn in ROW_NAMES:
-			if rn in context:
-				temp_dic[rn].append(context[rn])
-			else:
-				temp_dic[rn].append("項目なし")
-
-	list_2d = [temp_dic[x] for x in ROW_NAMES]
-	table = pd.DataFrame(list_2d, columns=COL_NAMES, index=ROW_NAMES)  
-
-	outcome_dic = {}
-	for context_type_name in ROW_IN_DOC.keys():
-		row_names_by_type = ROW_IN_DOC[context_type_name]
-		list_2d = [temp_dic[rn] if rn in temp_dic else ["項目なし"]*len(COL_NAMES) for rn in row_names_by_type]
-		re_idx_table = pd.DataFrame(list_2d, columns=COL_NAMES, index=row_names_by_type)
-		outcome_dic[context_type_name] = re_idx_table
-
-	return table, outcome_dic, COL_NAMES, ROW_NAMES
 
 
 def compare_data(Outcome_dic, Target_name):
@@ -150,6 +121,72 @@ def compare_data(Outcome_dic, Target_name):
 	return result_dic
 
 
+@st.cache
+def get_data(Data_name):
+	with context_DATA_dic[Data_name].open("r", encoding='utf-8') as fr:
+		main_dic = json.load(fr)
+	COL_NAMES = sorted(list(main_dic.keys()))
+	all_context_keys = sum([list(main_dic[c].keys()) for c in COL_NAMES], [])
+	ROW_NAMES = list(set(all_context_keys))
+
+	temp_dic = {x:[] for x in ROW_NAMES}
+	for cn in COL_NAMES:
+		context = main_dic[cn]
+		for rn in ROW_NAMES:
+			if rn in context:
+				temp_dic[rn].append(context[rn])
+			else:
+				temp_dic[rn].append("項目なし")
+
+	list_2d = [temp_dic[x] for x in ROW_NAMES]
+	table = pd.DataFrame(list_2d, columns=COL_NAMES, index=ROW_NAMES)  
+
+	outcome_dic = {}
+	for context_type_name in ROW_IN_DOC.keys():
+		row_names_by_type = ROW_IN_DOC[context_type_name]
+		list_2d = [temp_dic[rn] if rn in temp_dic else ["項目なし"]*len(COL_NAMES) for rn in row_names_by_type]
+		re_idx_table = pd.DataFrame(list_2d, columns=COL_NAMES, index=row_names_by_type)
+		outcome_dic[context_type_name] = re_idx_table
+	return table, outcome_dic
+
+
+@st.cache
+def get_editor_data(Editor_names):
+	result_dic = {edi:{} for edi in Editor_names}
+	for d_name in context_DATA_dic.keys():
+		with context_DATA_dic[d_name].open("r", encoding='utf-8') as fr:
+			data_dic = json.load(fr)
+		for edi in Editor_names:
+			if result_dic[edi] == {}:
+				result_dic[edi] = {d_name: data_dic[edi]}
+			else:
+				result_dic[edi][d_name] = data_dic[edi]
+
+	outcome_dic = {}
+	for edi in Editor_names:
+		col_names = list(context_DATA_dic.keys())
+
+		tbdic_by_type = {}
+		for ct_type_name in ROW_IN_DOC.keys():
+			row_names_by_type = ROW_IN_DOC[ct_type_name]
+			temp_dic = {x:[] for x in row_names_by_type}
+			for d_name in context_DATA_dic.keys():
+				context = result_dic[edi][d_name]
+				for rn in row_names_by_type:
+					if rn in context:
+						temp_dic[rn].append(context[rn])
+					else:
+						temp_dic[rn].append("項目なし")
+
+
+			list_2d = [temp_dic[rn] if rn in temp_dic else ["項目なし"]*len(col_names)
+						for rn in row_names_by_type]
+			table = pd.DataFrame(list_2d, columns=col_names, index=row_names_by_type)
+			tbdic_by_type[ct_type_name] = table
+
+		outcome_dic[edi] = tbdic_by_type
+
+	return outcome_dic
 
 
 
@@ -158,20 +195,38 @@ def compare_data(Outcome_dic, Target_name):
 
 # サイドバーの設定-------------------------
 
-if "初期状態" in context_DATA_dic:
-	idx = list(context_DATA_dic.keys()).index("初期状態")
-else:
-	idx = 0
-target_name = st.sidebar.radio("Blender の状態", list(context_DATA_dic.keys()), index=idx)
-st.sidebar.markdown("---------------------")
+base_is_editor = st.sidebar.checkbox("エディタを基準にして比較を行う", False)
 
-compare_dic = {
-		f"「{x.split(' + ')[0]}」と比較" if x!=target_name else "比較しない":x
-		for x in context_DATA_dic.keys() }
-compare_name = st.sidebar.radio("2つの状態の比較",
-				sorted(list(compare_dic.keys()),reverse=True))
+if base_is_editor:
+	edi_names = st.sidebar.multiselect("エディタ", COL_NAMES, default=[])
+
+	if "初期状態" in context_DATA_dic:
+		shown_datas = ["初期状態"] + [x for x in context_DATA_dic.keys() if x != "初期状態"]
+	else:
+		shown_datas = list(context_DATA_dic.keys())
+	method = st.sidebar.radio("表示する状態の選択方法", ["Checkbox", "Multi Select"])
+	if method == "Checkbox":
+		data_checks = [st.sidebar.checkbox(name, True) for idx, name in enumerate(shown_datas)]
+		data_bools = [x==True for x in data_checks]
+		active_datas = [n for b, n in zip(data_bools, shown_datas) if b]
+	else:
+		active_datas = st.sidebar.multiselect("状態", shown_datas, default=shown_datas)
+else:
+	if "初期状態" in context_DATA_dic:
+		idx = list(context_DATA_dic.keys()).index("初期状態")
+	else:
+		idx = 0
+	target_name = st.sidebar.radio("Blender の状態", list(context_DATA_dic.keys()), index=idx)
+	st.sidebar.markdown("---------------------")
+
+	compare_dic = {
+			f"「{x.split(' + ')[0]}」と比較" if x!=target_name else "比較しない":x
+			for x in context_DATA_dic.keys() }
+	compare_name = st.sidebar.radio("2つの状態の比較",
+					sorted(list(compare_dic.keys()),reverse=True))
 
 # データフレームの表示-------------------------
+
 
 def color_style(val):
 	color_dic = {"項目なし":'gray', "None":'red', "[]":'blue'}
@@ -186,60 +241,90 @@ def color_style(val):
 * 各エリアにオペレーターを設置し、`bpy.context.copy()` を保存
 """
 
-#-- API Doc 準拠 -------------------
 
-"""
-## ◇ API Doc の分類に準拠した表
-`項目なし`は、そのエリアの context に該当項目が含まれないことを示す
-"""
+if base_is_editor:
+	"""
+	## ◇ エディタを基準にして比較を行う
+	`項目なし`は、そのエリアの context に該当項目が含まれないことを示す
+	"""
 
-is_limit = st.checkbox("Global と Screen 以外は関連するエディタのみ表示", True)
+	if edi_names:
+		edi_dic = get_editor_data(edi_names)
+		REV = {v[0]:k for k,v in EDITORS_BY_TYPE.items()}
+		for edi in edi_dic.keys():
+			st.markdown(f"### ◇ {edi}")
+			ct_types = ["Global Context", "Screen Context"]
+			if edi in REV:
+				ct_types = ct_types + [REV[edi]]
 
-table, outcome_dic, COL_NAMES, ROW_NAMES = get_data(Data_name=target_name)
-diff_result = None
+			for context_type in ct_types:
+				st.markdown(f"#### ◇ {context_type}")
+				partly_table = edi_dic[edi][context_type]
+				df = partly_table.reindex(columns=active_datas)
+				st.dataframe(df.style.applymap(color_style))
 
-if compare_name != "比較しない":
-	comp_target = compare_dic[compare_name]
-	diff_result = compare_data(outcome_dic, comp_target)
+				diff_dic = extract_diff_dict(partly_table)
+				with st.beta_expander("差異"):
+					st.write(diff_dic)
+					st.markdown("---------------")
 
+			st.markdown(f"---------")
 
-for context_type in outcome_dic.keys():
-	st.markdown(f"### ◇ {context_type}")
-	partly_table = outcome_dic[context_type]
-	if is_limit:
-		if context_type in EDITORS_BY_TYPE:
-			partly_table = partly_table[EDITORS_BY_TYPE[context_type]]
-	st.dataframe(partly_table.style.applymap(color_style))
+else:
+	#-- API Doc 準拠 -------------------
 
-	if diff_result is not None:
-		partly_compared = diff_result[context_type]
-		with st.beta_expander("比較結果　(D = bpy.data,　obs = objects,　bpy_struct において省略あり)", expanded=True):
-			st.write(partly_compared)
-			
+	"""
+	## ◇ API Doc の分類に準拠した表
+	`項目なし`は、そのエリアの context に該当項目が含まれないことを示す
+	"""
+
+	is_limit = st.checkbox("Global と Screen 以外は関連するエディタのみ表示", True)
 	
-	if len(partly_table.columns) >= 2:
-		partly_row_names = partly_table.index.tolist()
-		diff_dic = extract_diff_dict(partly_table, partly_row_names, COL_NAMES)
-		with st.beta_expander("エディタ間の差異 (area, region, space_data, active_operator は除く)"):
-			st.write(diff_dic)
-			st.markdown("---------------")
+	table, outcome_dic = get_data(Data_name=target_name)
+	diff_result = None
+
+	if compare_name != "比較しない":
+		comp_target = compare_dic[compare_name]
+		diff_result = compare_data(outcome_dic, comp_target)
 
 
-#-- 集計表 -------------------
+	for context_type in outcome_dic.keys():
+		st.markdown(f"### ◇ {context_type}")
+		partly_table = outcome_dic[context_type]
+		if is_limit:
+			if context_type in EDITORS_BY_TYPE:
+				partly_table = partly_table[EDITORS_BY_TYPE[context_type]]
+		st.dataframe(partly_table.style.applymap(color_style))
+
+		if diff_result is not None:
+			partly_compared = diff_result[context_type]
+			with st.beta_expander("比較結果　(D = bpy.data,　obs = objects,　bpy_struct において省略あり)", expanded=True):
+				st.write(partly_compared)
+				
+		
+		if len(partly_table.columns) >= 2:
+			partly_row_names = partly_table.index.tolist()
+			diff_dic = extract_diff_dict(partly_table)
+			with st.beta_expander("エディタ間の差異 (area, region, space_data, active_operator は除く)"):
+				st.write(diff_dic)
+				st.markdown("---------------")
 
 
-st.markdown("------------------")
-with st.beta_expander("◇ 各エリアで取得した context の一覧表"):
-	st.markdown("`項目なし`は、そのエリアの context に該当項目が含まれないことを示す")
-	st.dataframe(table.style.applymap(color_style))
+	#-- 集計表 -------------------
+
+
+	st.markdown("------------------")
+	with st.beta_expander("◇ 各エリアで取得した context の一覧表"):
+		st.markdown("`項目なし`は、そのエリアの context に該当項目が含まれないことを示す")
+		st.dataframe(table.style.applymap(color_style))
 
 
 
-#---- エディタによる違い 全体 ----------------------
+	#---- エディタによる違い 全体 ----------------------
 
 
-st.markdown("------------------")
-with st.beta_expander("◇ エディタ間の差異：全体 (area, region, space_data, active_operator は除く)", expanded=False):
+	st.markdown("------------------")
+	with st.beta_expander("◇ エディタ間の差異：全体 (area, region, space_data, active_operator は除く)", expanded=False):
 
-	all_diff_dic = extract_diff_dict(table, ROW_NAMES, COL_NAMES)
-	st.write(all_diff_dic)
+		all_diff_dic = extract_diff_dict(table)
+		st.write(all_diff_dic)
